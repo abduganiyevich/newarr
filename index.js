00b3bf809349f1f0b135749aece45da58dcd4fed
abduganiyevich <abduganiyevyahyobek555@gmail.com>
2023-10-25 21:19:24 +0500

    hello

---
+++ index.js
@@ -0,0 +1,66 @@
+//  2-masala
+
+// let arr=[1,2,3,5,6,7,8,9];
+// const newArr=arr.filter((valus, index,arr)=>{
+//   return index%2==0;
+// })
+// console.log(newArr);
+
+/////////////////////////////////////////////////////////////////
+
+// 3-masala
+
+// let arr=[1,2,3,5,6,7,8,9];
+// const newArr=arr.map((valus, index,arr)=>{
+//   return valus**3;
+// })
+// console.log(newArr);
+
+//////////////////////////////////////////////////////////////////////
+
+// 4-masala
+
+// let arr=[1,2,3,,5,6,7,8,9,10];
+
+// let c=arr.some((value)=>{
+//   return value%5===0;
+// });
+// console.log(c)
+
+//////////////////////////////////////////////////////////////////////////
+// 5-masala
+
+// let arr=[1,2,3,5,6,7,8,9];
+// let newArr=arr.forEach((valus, index,arr)=>{
+//   if(valus%3===0){
+//     console.log( valus);
+//   }
+// }) 
+//////////////////////////////////////////////////////////////////////
+
+// 6-masala 
+
+// let arr=[1,2,3,5,6,7,8,9];
+// let newArr=arr.findIndex((element)=>{
+// return element%5===0;
+// });
+// console.log(newArr);
+//////////////////////////////////////////////////////////////////////
+
+// 7-masala
+
+// let arr=[1,2,3,,5,6,7,8,9,10];
+// let c=arr.includes(2);
+// console.log(c)
+
+// 9-misol
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// let sum=0;
+// for (const item of arr) {
+//   if(item%2===0){
+//     sum=sum+item**2;
+//     console.log(item**2)
+//   }
+// }
+// console.log(sum);
